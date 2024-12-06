a1452e5de69d83f2b96d37d507560c0609af67d2
AhmedxTamer <medotamer200490@gmail.com>
2024-12-06 18:58:43 +0200

    not yettt

--- src/connect/hub/BackEndContentCreation/User.java
+++ src/connect/hub/BackEndContentCreation/User.java
@@ -2,6 +2,7 @@ package connect.hub.BackEndContentCreation;
 
 
 import java.time.LocalDate;
+import java.util.Date;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -10,13 +11,72 @@ import java.time.LocalDate;
 
 /**
  *
- * @author Lenovo
+ * @author rawan
  */
 public class User {
-    private String userId;
-    private String username;
-    private String email;
-    private String password;
-    private LocalDate dateOfBirth;
-    private boolean status;
+String userId; 
+String email;   
+String username; 
+Date DOB;  
+boolean Status;
+String hashedPassword;  
+
+    public User( String email, String username, String hashedPassword, Date DOB) {
+     
+        this.email = email;
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        this.DOB = DOB;
+        this.Status = Status;
+    }
+
+
+//    public void setUserId(String userId) {
+//        this.userId = userId;
+//    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+
+    public void setStatus(boolean Status) {
+        this.Status = Status;
+    }
+
+//    public String getUserId() {
+//        return userId;
+//    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public boolean isOnline() {
+        return Status;
+    }
+
 }
--- src/connect/hub/ConnectHub.java
+++ src/connect/hub/ConnectHub.java
@@ -1,12 +1,8 @@
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package connect.hub;
-
-import connect.hub.BackEndContentCreation.FriendManager;
 
 /**
  *
@@ -18,7 +14,6 @@ public class ConnectHub {
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        FriendManager friendManager = FriendManager.getInstance();
-        friendManager.sendFriendRequest("us", "ur");
+    
     }
 }
