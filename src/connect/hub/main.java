d9ae012c7017a554186738a4b444abed1b84b90a
AhmedxTamer <medotamer200490@gmail.com>
2024-12-11 19:20:23 +0200

    testing

---
+++ src/connect/hub/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connect.hub;
+
+import connect.hub.FrontEndFriendManagement.FriendManagementInterface;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class main {
+    public static void main(String[] args){
+        FriendManagementInterface fmi = new FriendManagementInterface();
+        fmi.showFrame();
+    }
+}
