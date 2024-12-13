58d842205d6860195e9a2a040c0c4554185c3018
Mohamed Elhofy <alhofymohamed@gmail.com>
2024-12-13 22:42:15 +0200

    Remove main in GroupAdminGui

--- src/GroupFront/GroupAdminGui.java
+++ src/GroupFront/GroupAdminGui.java
@@ -139,9 +139,6 @@ public class GroupAdminGui extends JFrame  {
             }
         });
     }
-    public static void main(String[] args) {
-        GroupPrimaryAdmin group =new GroupPrimaryAdmin("ff","fff","D:\\\\OOP\\\\Connect-Hub\\\\my_photo\\\\WhatsApp Image 2023-12-14 at 13.43.35_7256691a.jpg");
-        new GroupAdminGui(group).setVisible(true);
-    }
+
 }
 
